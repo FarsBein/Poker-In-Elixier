6aee7f1f60ea24e47c74a7898ff223741e0417f1
Fars Bein <53714581+FarsBein@users.noreply.github.com>
2021-03-03 22:37:21 -0500

    done checkSuit

--- Poker.ex
+++ Poker.ex
@@ -2,8 +2,7 @@ defmodule Poker do
 
   def deal(cards) do translate(cards) end
   defp deal([], hand1, hand2, pool) do
-    finalCards = [hand1] ++ [hand2] ++ [pool]
-    IO.inspect finalCards
+    compare(hand1 ++ pool, hand2 ++ pool)
   end
   defp deal([head | tail], hand1, hand2, _) do
     if length(hand1) < 1 do
@@ -36,17 +35,50 @@ defmodule Poker do
       end
   end
 
-  # def compare(handPool1, handPool2) do
+  def compare(handPool1, handPool2) do
+    [hand1, hand1rank, tieBreaker1] = checkSuit handPool1
+    [hand2, hand2rank, tieBreaker2] = checkSuit handPool2
 
-  # end
+    if hand1rank > hand2rank do
+      IO.inspect hand1
+      IO.inspect hand1rank
+    end
 
+    if hand1rank < hand2rank do
+      IO.inspect hand2
+      IO.inspect hand2rank
+    end
 
-  # def checkSuit(cards) do
-  #   {num,suit} = String.split_at(List.first(cards), 1)
-  #   Enum.filter(cards, fn card ->
-  #     {Cnum,Csuit} = String.split_at(card, 1)
-  #     Csuit == suit
-  #   end)
-  # end
+    if hand1rank == hand2rank do
+      if tieBreaker1 > tieBreaker2 do
+        IO.inspect hand1
+        IO.inspect hand1rank
+        IO.inspect tieBreaker1
+      end
+      if tieBreaker1 < tieBreaker2 do
+        IO.inspect hand2
+        IO.inspect hand2rank
+        IO.inspect tieBreaker2
+      end
+    end
 
+  end
+
+  def checkSuit(hand) do
+    condition = checkSuit(hand, (tl (hd hand)))
+    if condition do
+      [1,3,1] #next fun that handles suit should be here
+    else
+      [hand,3,(hd (hd hand))] #next fun that handles non-suit should be here
+    end
+  end
+  defp checkSuit([h | t], suit) do
+    if (tl h) == suit do
+      checkSuit(t, suit)
+    else
+      false
+    end
+  end
+  defp checkSuit([],_) do true end
+  
 end
---
+++ Testing.ex
@@ -0,0 +1,25 @@
+defmodule Testing do
+  # def aReturn(c,_) do
+  #   aReturn([1,2,3])
+  # end
+
+  # def aReturn([h|t]) do
+  #   aReturn(t)
+  # end
+
+  # def aReturn([]) do
+  #   [1,2,3,4,5,6,7,8,9,10]
+  # end
+
+  # def bReturn() do
+  #   c = aReturn("true","dd")
+  #   IO.inspect c
+  # end
+
+  def caseTest() do
+    case 5 do
+      x >= 5 -> "bigger than 5"
+      _ -> "smaller than 5"
+    end
+  end
+end
